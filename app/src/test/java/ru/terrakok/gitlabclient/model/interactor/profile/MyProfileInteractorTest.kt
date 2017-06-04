44875c85b4f77a63713dab3ceb37558d6694d941
Artur Badretdinov <artursletter@gmail.com>
2017-06-04 11:21:57 +0300

    Add test for unauthorized user. Refactor tests, add SetUp() method

--- app/src/test/java/ru/terrakok/gitlabclient/model/interactor/profile/MyProfileInteractorTest.kt
+++ app/src/test/java/ru/terrakok/gitlabclient/model/interactor/profile/MyProfileInteractorTest.kt
@@ -3,6 +3,7 @@ package ru.terrakok.gitlabclient.model.interactor.profile
 import io.reactivex.Observable
 import io.reactivex.Single
 import io.reactivex.observers.TestObserver
+import org.junit.Before
 import org.junit.Test
 import org.mockito.Mockito.*
 import ru.terrakok.gitlabclient.entity.app.MyUserInfo
@@ -17,19 +18,29 @@ import java.util.*
  */
 class MyProfileInteractorTest {
 
-    @Test
-    fun get_user_info() {
-        val testUser = User(1L, null, null, null, null, null,
+    lateinit var profileRepo:ProfileRepository
+    lateinit var authRepo:AuthRepository
+
+    var testUser:User? = null
+    lateinit var testServer:String
+
+    @Before
+    fun setUp() {
+        testUser = User(1L, null, null, null, null, null,
                 null, Date(), false, null, null, null, null, null,
                 null, null, Date(), Date(), 0L, 0L, Date(), null, false, false, false, false)
-        val testServer = "Test server"
+        testServer = "Test server"
+        profileRepo = mock(ProfileRepository::class.java)
+        authRepo = mock(AuthRepository::class.java)
+    }
+
+    @Test
+    fun get_user_info() {
         val testUserInfo = MyUserInfo(testUser, testServer)
 
-        val profileRepo = mock(ProfileRepository::class.java)
         `when`(profileRepo.getMyProfile()).thenReturn(Single.just(testUser))
         `when`(profileRepo.getMyServerName()).thenReturn(testServer)
 
-        val authRepo = mock(AuthRepository::class.java)
         `when`(authRepo.getSignState()).thenReturn(Observable.just(true))
 
         val interactor = MyProfileInteractor(authRepo, profileRepo)
@@ -40,6 +51,28 @@ class MyProfileInteractorTest {
         verify(profileRepo, times(1)).getMyProfile()
         verify(profileRepo, times(1)).getMyServerName()
 
+        verify(authRepo, times(1)).getSignState()
+
+        testObserver
+                .assertValueCount(1)
+                .assertValue(testUserInfo)
+                .assertNoErrors()
+    }
+
+    @Test
+    fun get_unauthorized_user_info() {
+        testUser = null
+        val testUserInfo = MyUserInfo(testUser, testServer)
+
+        `when`(profileRepo.getMyServerName()).thenReturn(testServer)
+        `when`(authRepo.getSignState()).thenReturn(Observable.just(false))
+
+        val interactor = MyProfileInteractor(authRepo, profileRepo)
+
+        val testObserver: TestObserver<MyUserInfo> = interactor.getMyProfile().test()
+        testObserver.awaitTerminalEvent()
+
+        verify(profileRepo, times(1)).getMyServerName()
         verify(authRepo, times(1)).getSignState()
 
         testObserver
