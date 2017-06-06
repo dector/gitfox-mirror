7e8d4d006b01ed1748e76c4cd89212c3e8176898
terrakok <terrakok@gmail.com>
2017-06-07 00:27:26 +0300

    added unit tests for MainProjectsListInteractor

---
+++ app/src/test/java/ru/terrakok/gitlabclient/model/interactor/project/TestSchedulers.kt
@@ -0,0 +1,7 @@
+package ru.terrakok.gitlabclient.model.interactor.project
+
+/**
+ * @author Konstantin Tskhovrebov (aka terrakok). Date: 28.05.17
+ */
+class TestSchedulers {
+}
---
+++ app/src/test/java/ru/terrakok/gitlabclient/model/interactor/projects/MainProjectsListInteractorTest.kt
@@ -0,0 +1,120 @@
+package ru.terrakok.gitlabclient.model.interactor.projects
+
+import com.nhaarman.mockito_kotlin.*
+import io.reactivex.Single
+import org.junit.Before
+import org.junit.Test
+import ru.terrakok.gitlabclient.entity.app.ProjectsListFilter
+import ru.terrakok.gitlabclient.model.repository.project.ProjectRepository
+
+/**
+ * @author Konstantin Tskhovrebov (aka terrakok) on 04.06.17.
+ */
+class MainProjectsListInteractorTest {
+
+    private lateinit var projectRepository: ProjectRepository
+    private val testFilter = ProjectsListFilter()
+    private val testPage = 0
+    private val testError = RuntimeException()
+
+    @Before
+    fun setUp() {
+        projectRepository = mock<ProjectRepository>()
+    }
+
+    @Test
+    fun getMainProjects_success() {
+        whenever(projectRepository.getProjectsList(any(), any(), any())).thenReturn(Single.just(emptyList()))
+
+        val interactor = MainProjectsListInteractor(projectRepository)
+
+        val testObserver = interactor.getMainProjects(testPage).test()
+        testObserver.awaitTerminalEvent()
+
+        val captor = argumentCaptor<ProjectsListFilter>()
+        verify(projectRepository).getProjectsList(captor.capture(), eq(testPage), any())
+        testObserver
+                .assertValueCount(1)
+                .assertNoErrors()
+    }
+
+    @Test
+    fun getMainProjects_error() {
+        whenever(projectRepository.getProjectsList(any(), any(), any())).thenReturn(Single.error(testError))
+
+        val interactor = MainProjectsListInteractor(projectRepository)
+
+        val testObserver = interactor.getMainProjects(testPage).test()
+        testObserver.awaitTerminalEvent()
+
+        val captor = argumentCaptor<ProjectsListFilter>()
+        verify(projectRepository).getProjectsList(captor.capture(), eq(testPage), any())
+        testObserver
+                .assertNoValues()
+                .assertError(testError)
+    }
+
+    @Test
+    fun getMyProjects_success() {
+        whenever(projectRepository.getProjectsList(any(), any(), any())).thenReturn(Single.just(emptyList()))
+
+        val interactor = MainProjectsListInteractor(projectRepository)
+
+        val testObserver = interactor.getMyProjects(testPage).test()
+        testObserver.awaitTerminalEvent()
+
+        val captor = argumentCaptor<ProjectsListFilter>()
+        verify(projectRepository).getProjectsList(captor.capture(), eq(testPage), any())
+        testObserver
+                .assertValueCount(1)
+                .assertNoErrors()
+    }
+
+    @Test
+    fun getMyProjects_error() {
+        whenever(projectRepository.getProjectsList(any(), any(), any())).thenReturn(Single.error(testError))
+
+        val interactor = MainProjectsListInteractor(projectRepository)
+
+        val testObserver = interactor.getMyProjects(testPage).test()
+        testObserver.awaitTerminalEvent()
+
+        val captor = argumentCaptor<ProjectsListFilter>()
+        verify(projectRepository).getProjectsList(captor.capture(), eq(testPage), any())
+        testObserver
+                .assertNoValues()
+                .assertError(testError)
+    }
+
+    @Test
+    fun getStarredProjects_success() {
+        whenever(projectRepository.getProjectsList(any(), any(), any())).thenReturn(Single.just(emptyList()))
+
+        val interactor = MainProjectsListInteractor(projectRepository)
+
+        val testObserver = interactor.getStarredProjects(testPage).test()
+        testObserver.awaitTerminalEvent()
+
+        val captor = argumentCaptor<ProjectsListFilter>()
+        verify(projectRepository).getProjectsList(captor.capture(), eq(testPage), any())
+        testObserver
+                .assertValueCount(1)
+                .assertNoErrors()
+    }
+
+    @Test
+    fun getStarredProjects_error() {
+        whenever(projectRepository.getProjectsList(any(), any(), any())).thenReturn(Single.error(testError))
+
+        val interactor = MainProjectsListInteractor(projectRepository)
+
+        val testObserver = interactor.getStarredProjects(testPage).test()
+        testObserver.awaitTerminalEvent()
+
+        val captor = argumentCaptor<ProjectsListFilter>()
+        verify(projectRepository).getProjectsList(captor.capture(), eq(testPage), any())
+        testObserver
+                .assertNoValues()
+                .assertError(testError)
+    }
+}
