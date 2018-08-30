2f5ff3e9327791539e76ae99175573b47f581d29
CraggyHaggy <eugeneshapovalov94@gmail.com>
2018-08-31 00:00:33 +0300

    Fixed bug associated with blinking items after update target list.

--- app/src/main/java/ru/terrakok/gitlabclient/ui/my/TargetsAdapter.kt
+++ app/src/main/java/ru/terrakok/gitlabclient/ui/my/TargetsAdapter.kt
@@ -4,16 +4,16 @@ import android.support.v7.util.DiffUtil
 import android.support.v7.widget.RecyclerView
 import com.hannesdorfmann.adapterdelegates3.ListDelegationAdapter
 import ru.terrakok.gitlabclient.entity.app.target.TargetHeader
-import ru.terrakok.gitlabclient.ui.global.list.DiffCallback
 import ru.terrakok.gitlabclient.ui.global.list.ProgressAdapterDelegate
 import ru.terrakok.gitlabclient.ui.global.list.ProgressItem
 import ru.terrakok.gitlabclient.ui.global.list.TargetHeaderAdapterDelegate
 
 class TargetsAdapter(
-        userClickListener: (Long) -> Unit,
-        clickListener: (TargetHeader) -> Unit,
-        private val nextListener: () -> Unit
+    userClickListener: (Long) -> Unit,
+    clickListener: (TargetHeader) -> Unit,
+    private val nextListener: () -> Unit
 ) : ListDelegationAdapter<MutableList<Any>>() {
+
     init {
         items = mutableListOf()
         delegatesManager.addDelegate(TargetHeaderAdapterDelegate(userClickListener, clickListener))
@@ -49,9 +49,45 @@ class TargetsAdapter(
 
     private fun isProgress() = items.isNotEmpty() && items.last() is ProgressItem
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int, payloads: MutableList<Any?>) {
+    override fun onBindViewHolder(
+        holder: RecyclerView.ViewHolder,
+        position: Int,
+        payloads: MutableList<Any?>
+    ) {
         super.onBindViewHolder(holder, position, payloads)
 
         if (position == items.size - 10) nextListener()
+    }
+
+    private inner class DiffCallback(
+        private val newItems: List<Any>,
+        private val oldItems: List<Any>
+    ) : DiffUtil.Callback() {
+
+        override fun getOldListSize() = oldItems.size
+        override fun getNewListSize() = newItems.size
+
+        override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+            val oldItem = oldItems[oldItemPosition]
+            val newItem = newItems[newItemPosition]
+
+            return if (newItem is TargetHeader && oldItem is TargetHeader) {
+                newItem.target == oldItem.target && newItem.targetId == oldItem.targetId
+                        && newItem.date == oldItem.date
+            } else {
+                newItem is ProgressItem && oldItem is ProgressItem
+            }
+        }
+
+        override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+            val oldItem = oldItems[oldItemPosition]
+            val newItem = newItems[newItemPosition]
+
+            return if (newItem is TargetHeader && oldItem is TargetHeader) {
+                newItem == oldItem
+            } else {
+                true
+            }
+        }
     }
 }
