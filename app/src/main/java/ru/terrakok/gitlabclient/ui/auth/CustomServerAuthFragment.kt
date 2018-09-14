8ea9c7e72c4070a9914265cd6ede398f24539d69
CraggyHaggy <eugeneshapovalov94@gmail.com>
2018-09-14 22:16:06 +0300

    Fix crash with base url for custom server auth.

--- app/src/main/java/ru/terrakok/gitlabclient/ui/auth/CustomServerAuthFragment.kt
+++ app/src/main/java/ru/terrakok/gitlabclient/ui/auth/CustomServerAuthFragment.kt
@@ -6,6 +6,7 @@ import android.support.design.widget.BottomSheetDialogFragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
 import android.webkit.URLUtil
 import android.widget.Toast
 import kotlinx.android.synthetic.main.fragment_custom_server_auth.*
@@ -36,6 +37,14 @@ class CustomServerAuthFragment : BottomSheetDialogFragment() {
             serverPathValue.setText(BuildConfig.ORIGIN_GITLAB_ENDPOINT)
         }
 
+        privateTokenValue.setOnEditorActionListener { _, actionId, _ ->
+            return@setOnEditorActionListener if (actionId == EditorInfo.IME_ACTION_DONE) {
+                login()
+                true
+            } else {
+                false
+            }
+        }
         loginButton.setOnClickListener { login() }
         cancelButton.setOnClickListener { dismiss() }
     }
@@ -50,7 +59,7 @@ class CustomServerAuthFragment : BottomSheetDialogFragment() {
         }
 
         if (URLUtil.isValidUrl(url)) {
-            listener.customLogin.invoke(url, token)
+            listener.customLogin.invoke(if (url.endsWith("/")) url else "$url/", token)
             dismiss()
         } else {
             Toast.makeText(this.context, getString(R.string.invalid_server_url), Toast.LENGTH_SHORT).show()
