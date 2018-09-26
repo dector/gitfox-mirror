d07d86016f4881fd3d4b19aab4fde8305aa957c6
Konstantin Tskhovrebov <k.tskhovrebov@redmadrobot.com>
2018-09-26 22:55:15 +0300

    Add first launch check and show privacy policy if needed.

--- app/src/main/java/ru/terrakok/gitlabclient/model/data/storage/Prefs.kt
+++ app/src/main/java/ru/terrakok/gitlabclient/model/data/storage/Prefs.kt
@@ -17,6 +17,9 @@ class Prefs @Inject constructor(
     private val KEY_SERVER_PATH = "ad_server_path"
     private val KEY_IS_OAUTH = "ad_is_oauth"
 
+    private val APP_DATA = "app_data"
+    private val KEY_FIRST_LAUNCH_TIME = "launch_ts"
+
     private fun getSharedPreferences(prefsName: String) = context.getSharedPreferences(prefsName, Context.MODE_PRIVATE)
 
     override var token: String?
@@ -35,5 +38,11 @@ class Prefs @Inject constructor(
         get() = getSharedPreferences(AUTH_DATA).getBoolean(KEY_IS_OAUTH, false)
         set(value) {
             getSharedPreferences(AUTH_DATA).edit().putBoolean(KEY_IS_OAUTH, value).apply()
+        }
+
+    var firstLaunchTimeStamp: Long?
+        get() = getSharedPreferences(APP_DATA).getLong(KEY_FIRST_LAUNCH_TIME, 0).takeIf { it > 0 }
+        set(value) {
+            getSharedPreferences(APP_DATA).edit().putLong(KEY_FIRST_LAUNCH_TIME, value ?: 0).apply()
         }
 }
