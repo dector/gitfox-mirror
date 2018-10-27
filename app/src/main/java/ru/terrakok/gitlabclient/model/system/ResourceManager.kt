dfb15dd6bab9163bbed6539d111c3f994646a49a
CraggyHaggy <eugeneshapovalov94@gmail.com>
2018-10-28 02:52:10 +0300

    Refactor MergeRequestFragment toolbar logic.

--- app/src/main/java/ru/terrakok/gitlabclient/model/system/ResourceManager.kt
+++ app/src/main/java/ru/terrakok/gitlabclient/model/system/ResourceManager.kt
@@ -9,4 +9,6 @@ import javax.inject.Inject
 class ResourceManager @Inject constructor(private val context: Context) {
 
     fun getString(id: Int) = context.getString(id)
+
+    fun getString(id: Int, vararg formatArgs: Any) = String.format(context.getString(id, *formatArgs))
 }
--- app/src/main/java/ru/terrakok/gitlabclient/presentation/mergerequest/info/MergeRequestInfoPresenter.kt
+++ app/src/main/java/ru/terrakok/gitlabclient/presentation/mergerequest/info/MergeRequestInfoPresenter.kt
@@ -1,12 +1,7 @@
 package ru.terrakok.gitlabclient.presentation.mergerequest.info
 
 import com.arellomobile.mvp.InjectViewState
-import io.reactivex.Single
-import io.reactivex.functions.BiFunction
-import ru.terrakok.gitlabclient.entity.Project
-import ru.terrakok.gitlabclient.entity.mergerequest.MergeRequest
 import ru.terrakok.gitlabclient.model.interactor.mergerequest.MergeRequestInteractor
-import ru.terrakok.gitlabclient.model.interactor.project.ProjectInteractor
 import ru.terrakok.gitlabclient.presentation.global.BasePresenter
 import ru.terrakok.gitlabclient.presentation.global.ErrorHandler
 import ru.terrakok.gitlabclient.presentation.global.MarkDownConverter
@@ -14,8 +9,6 @@ import ru.terrakok.gitlabclient.toothpick.PrimitiveWrapper
 import ru.terrakok.gitlabclient.toothpick.qualifier.MergeRequestId
 import ru.terrakok.gitlabclient.toothpick.qualifier.ProjectId
 import javax.inject.Inject
-
-private typealias MergeRequestLinker = BiFunction<Pair<MergeRequest, CharSequence>, Project, MergeRequestInfoView.MergeRequestInfo>
 
 /**
  * Created by Konstantin Tskhovrebov (aka @terrakok) on 05.01.18.
@@ -25,7 +18,6 @@ class MergeRequestInfoPresenter @Inject constructor(
     @ProjectId projectIdWrapper: PrimitiveWrapper<Long>,
     @MergeRequestId mrIdWrapper: PrimitiveWrapper<Long>,
     private val mrInteractor: MergeRequestInteractor,
-    private val projectInteractor: ProjectInteractor,
     private val mdConverter: MarkDownConverter,
     private val errorHandler: ErrorHandler
 ) : BasePresenter<MergeRequestInfoView>() {
@@ -36,22 +28,17 @@ class MergeRequestInfoPresenter @Inject constructor(
     override fun onFirstViewAttach() {
         super.onFirstViewAttach()
 
-        Single
-            .zip(
-                mrInteractor
-                    .getMergeRequest(projectId, mrId)
-                    .flatMap { mr ->
-                        mdConverter
-                            .markdownToSpannable(mr.description)
-                            .map { Pair(mr, it) }
-                    },
-                projectInteractor.getProject(projectId),
-                MergeRequestLinker { (mr, html), project -> MergeRequestInfoView.MergeRequestInfo(mr, project, html) }
-            )
-            .doOnSubscribe { viewState.showProgress(true) }
-            .doAfterTerminate { viewState.showProgress(false) }
+        mrInteractor
+            .getMergeRequest(projectId, mrId)
+            .flatMap { mr ->
+                mdConverter
+                    .markdownToSpannable(mr.description)
+                    .map { Pair(mr, it) }
+            }
+            .doOnSubscribe { viewState.showEmptyProgress(true) }
+            .doAfterTerminate { viewState.showEmptyProgress(false) }
             .subscribe(
-                { viewState.showInfo(it) },
+                { (mr, mdDescription) -> viewState.showInfo(mr, mdDescription) },
                 { errorHandler.proceed(it, { viewState.showMessage(it) }) }
             )
             .connect()
