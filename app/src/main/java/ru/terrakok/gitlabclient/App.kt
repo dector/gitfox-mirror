82b2e9970245bebf89c66e304d87d0f5f5254951
CraggyHaggy <eugeneshapovalov94@gmail.com>
2018-11-01 09:43:21 +0300

    Fix crash with restoring toothpick modules.

--- app/src/main/java/ru/terrakok/gitlabclient/App.kt
+++ app/src/main/java/ru/terrakok/gitlabclient/App.kt
@@ -16,6 +16,7 @@ import toothpick.Toothpick
 import toothpick.configuration.Configuration
 import toothpick.registries.FactoryRegistryLocator
 import toothpick.registries.MemberInjectorRegistryLocator
+import java.util.*
 
 /**
  * @author Konstantin Tskhovrebov (aka terrakok) on 26.03.17.
@@ -24,7 +25,7 @@ class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        appHashCode = hashCode()
+        appHashCode = UUID.randomUUID().hashCode()
 
         initLogger()
         initFabric()
