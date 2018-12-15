d0d8334c3f244b67a19fd6a3cae1713d29097342
Максим Мялкин <maxmyalkin@gmail.com>
2018-12-15 04:02:24 +0300

    Add label item markup.

--- app/src/main/java/ru/terrakok/gitlabclient/ui/project/labels/ProjectLabelAdapterDelegate.kt
+++ app/src/main/java/ru/terrakok/gitlabclient/ui/project/labels/ProjectLabelAdapterDelegate.kt
@@ -1,5 +1,9 @@
 package ru.terrakok.gitlabclient.ui.project.labels
 
+import android.annotation.SuppressLint
+import android.graphics.Color
+import android.support.v4.content.ContextCompat
+import android.support.v4.graphics.ColorUtils
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
@@ -8,6 +12,7 @@ import kotlinx.android.synthetic.main.item_label.view.*
 import ru.terrakok.gitlabclient.R
 import ru.terrakok.gitlabclient.entity.Label
 import ru.terrakok.gitlabclient.extension.inflate
+import ru.terrakok.gitlabclient.extension.setBackgroundTintByColor
 
 /**
  * @author Maxim Myalkin (MaxMyalkin) on 15.12.2018.
@@ -36,10 +41,31 @@ class ProjectLabelAdapterDelegate : AdapterDelegate<MutableList<Any>>() {
 
     private class Holder(view: View) : RecyclerView.ViewHolder(view) {
 
-        fun bind(item: Label) {
-            with(itemView) {
-                labelTitleTextView.text = item.name
+        @SuppressLint("SetTextI18n")
+        fun bind(item: Label) = with(itemView) {
+            labelTitleTextView.text = item.name
+            labelDescriptionTextView.text = item.description
+            labelIssueCountTextView.text = "${item.closedIssuesCount + item.openIssuesCount}"
+            labelMrCountTextView.text = "${item.openMergeRequestsCount}"
+            setLabelColor(item.color)
+
+        }
+
+        private fun setLabelColor(color: String) = with(itemView.labelTitleTextView) {
+            val labelColor = Color.parseColor(color)
+            val textColor = when {
+                isColorDark(labelColor) -> ContextCompat.getColor(context, R.color.white)
+                else -> ContextCompat.getColor(context, R.color.primary_text)
             }
+
+            setBackgroundTintByColor(labelColor)
+            setTextColor(textColor)
+        }
+
+        private fun isColorDark(color: Int) = ColorUtils.calculateLuminance(color) < DARK_COLOR_THRESHOLD
+
+        companion object {
+            private const val DARK_COLOR_THRESHOLD = .5f
         }
     }
 
