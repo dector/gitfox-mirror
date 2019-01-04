685a0a64b109de0210ce5e21f9c4fa328b9c273e
illuzor <illuzor@gmail.com>
2019-01-05 00:27:50 +0300

    ProjectsAdapter separated from ProjectsListFragment (#129)

--- app/src/main/java/ru/terrakok/gitlabclient/ui/projects/ProjectsListFragment.kt
+++ app/src/main/java/ru/terrakok/gitlabclient/ui/projects/ProjectsListFragment.kt
@@ -2,10 +2,8 @@ package ru.terrakok.gitlabclient.ui.projects
 
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
 import com.arellomobile.mvp.presenter.InjectPresenter
 import com.arellomobile.mvp.presenter.ProvidePresenter
-import com.hannesdorfmann.adapterdelegates3.ListDelegationAdapter
 import kotlinx.android.synthetic.main.layout_base_list.*
 import kotlinx.android.synthetic.main.layout_zero.*
 import ru.terrakok.gitlabclient.R
@@ -18,9 +16,6 @@ import ru.terrakok.gitlabclient.toothpick.PrimitiveWrapper
 import ru.terrakok.gitlabclient.toothpick.qualifier.ProjectListMode
 import ru.terrakok.gitlabclient.ui.global.BaseFragment
 import ru.terrakok.gitlabclient.ui.global.ZeroViewHolder
-import ru.terrakok.gitlabclient.ui.global.list.ProgressAdapterDelegate
-import ru.terrakok.gitlabclient.ui.global.list.ProgressItem
-import ru.terrakok.gitlabclient.ui.global.list.ProjectAdapterDelegate
 import toothpick.Scope
 import toothpick.config.Module
 
@@ -29,7 +24,7 @@ import toothpick.config.Module
  */
 class ProjectsListFragment : BaseFragment(), ProjectsListView {
 
-    private val adapter = ProjectsAdapter()
+    private val adapter = ProjectsAdapter({ presenter.loadNextProjectsPage() }, { presenter.onProjectClicked(it) })
     private var zeroViewHolder: ZeroViewHolder? = null
 
     override val layoutRes = R.layout.fragment_projects
@@ -98,41 +93,6 @@ class ProjectsListFragment : BaseFragment(), ProjectsListView {
     }
 
     override fun onBackPressed() = presenter.onBackPressed()
-
-    inner class ProjectsAdapter : ListDelegationAdapter<MutableList<Any>>() {
-        init {
-            items = mutableListOf()
-            delegatesManager.addDelegate(ProjectAdapterDelegate({ presenter.onProjectClicked(it.id) }))
-            delegatesManager.addDelegate(ProgressAdapterDelegate())
-        }
-
-        fun setData(projects: List<Project>) {
-            val progress = isProgress()
-
-            items.clear()
-            items.addAll(projects)
-            if (progress) items.add(ProgressItem())
-
-            notifyDataSetChanged()
-        }
-
-        fun showProgress(isVisible: Boolean) {
-            val currentProgress = isProgress()
-
-            if (isVisible && !currentProgress) items.add(ProgressItem())
-            else if (!isVisible && currentProgress) items.remove(items.last())
-
-            notifyDataSetChanged()
-        }
-
-        private fun isProgress() = items.isNotEmpty() && items.last() is ProgressItem
-
-        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int, payloads: MutableList<Any?>) {
-            super.onBindViewHolder(holder, position, payloads)
-
-            if (position == items.size - 10) presenter.loadNextProjectsPage()
-        }
-    }
 
     companion object {
         private const val ARG_MODE = "plf_mode"
