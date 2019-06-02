625507e8d057eec541e1d93a4439268c427779b9
terrakok <terrakok@gmail.com>
2019-06-02 23:54:26 +0300

    Update OffsetDateTimeDeserializer for all ISO formats.

--- app/src/main/java/ru/terrakok/gitlabclient/model/data/server/deserializer/OffsetDateTimeDeserializer.kt
+++ app/src/main/java/ru/terrakok/gitlabclient/model/data/server/deserializer/OffsetDateTimeDeserializer.kt
@@ -4,16 +4,13 @@ import com.google.gson.JsonDeserializationContext
 import com.google.gson.JsonDeserializer
 import com.google.gson.JsonElement
 import org.threeten.bp.OffsetDateTime
-import org.threeten.bp.format.DateTimeFormatter
 import java.lang.reflect.Type
 
 class OffsetDateTimeDeserializer : JsonDeserializer<OffsetDateTime> {
-
-    private val dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSX")
 
     override fun deserialize(
         json: JsonElement,
         typeOfT: Type,
         context: JsonDeserializationContext?
-    ): OffsetDateTime = OffsetDateTime.parse(json.asJsonPrimitive.asString, dateTimeFormatter)
+    ): OffsetDateTime = OffsetDateTime.parse(json.asJsonPrimitive.asString)
 }
