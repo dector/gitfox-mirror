7285d3af12b306ebae5b78f2af555e9cc45d7a64
terrakok <terrakok@gmail.com>
2020-04-06 18:27:28 +0300

    Add serialize method for ColorDeserializer.

--- sdk/src/commonMain/kotlin/gitfox/model/data/server/deserializer/ColorDeserializer.kt
+++ sdk/src/commonMain/kotlin/gitfox/model/data/server/deserializer/ColorDeserializer.kt
@@ -2,14 +2,16 @@ package gitfox.model.data.server.deserializer
 
 import gitfox.entity.Color
 import kotlinx.serialization.Decoder
+import kotlinx.serialization.Encoder
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializer
 
 @Serializer(forClass = Color::class)
 internal object ColorDeserializer : KSerializer<Color> {
-    override fun deserialize(decoder: Decoder): Color {
-        // The color of the label given in 6-digit hex notation with leading ‘#’ sign (e.g. #FFAABB)
-        // or one of the CSS color names. So according to CSS color names it can be named differently on Android.
-        return Color(decoder.decodeString())
+    override fun serialize(encoder: Encoder, value: Color) {
+        encoder.encodeString(value.name)
     }
+
+    override fun deserialize(decoder: Decoder): Color =
+        Color(decoder.decodeString())
 }
