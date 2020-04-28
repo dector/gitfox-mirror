9af1c67569945535f97c9dce463b6b8da56e57ed
Eugene Saturov <esaturov@gmail.com>
2020-04-28 22:45:22 +0300

    SDK aar included to Android app

--- GitFox Flutter Sample/android/app/src/main/kotlin/com/example/gitfox/MainActivity.kt
+++ GitFox Flutter Sample/android/app/src/main/kotlin/com/example/gitfox/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.gitfox
 
+import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.annotation.NonNull;
+import gitfox.SDK
+import gitfox.create
+import gitfox.entity.app.session.OAuthParams
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+
+    private lateinit var sdk: SDK
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        initSDK()
+    }
+
+    private fun MainActivity.initSDK() {
+        this.sdk = SDK.create(
+                context,
+                oAuthParams = OAuthParams(
+                        "https://gitlab.com/",
+                        "appId",
+                        "appKey",
+                        "redirectUrl"
+                ),
+                isDebug = BuildConfig.DEBUG
+        )
+    }
+
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine);
     }
