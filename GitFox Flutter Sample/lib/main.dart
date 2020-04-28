6dcbc8ddf60d92dd5357b22f329d6dd742b1ba4b
Eugene Saturov <esaturov@gmail.com>
2020-04-28 21:39:55 +0300

    Flutter project init

---
+++ GitFox Flutter Sample/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'GitFox Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("GitFox Demo"),
+      ),
+    );
+  }
+}
