76f9cc83c557a0d5fbab6e188c9517f8c5084fe3
Eugene Saturov <esaturov@gmail.com>
2020-04-30 09:44:15 +0300

    OAuth url retrieving (with failure)

--- GitFox Flutter Sample/android/app/src/main/kotlin/com/example/gitfox/MainActivity.kt
+++ GitFox Flutter Sample/android/app/src/main/kotlin/com/example/gitfox/MainActivity.kt
@@ -6,20 +6,27 @@ import androidx.annotation.NonNull;
 import gitfox.SDK
 import gitfox.create
 import gitfox.entity.app.session.OAuthParams
+import gitfox.model.interactor.SessionInteractor
+import io.flutter.BuildConfig
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity : FlutterActivity() {
 
-    private lateinit var sdk: SDK
+    private val CHANNEL = "gitfox/oauth-url"
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    private var sdk: SDK? = null
+
+    private var sessionInteractor: SessionInteractor? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         initSDK()
     }
 
-    private fun MainActivity.initSDK() {
+    private fun initSDK() {
         this.sdk = SDK.create(
                 context,
                 oAuthParams = OAuthParams(
@@ -30,9 +37,23 @@ class MainActivity : FlutterActivity() {
                 ),
                 isDebug = BuildConfig.DEBUG
         )
+
+        this.sessionInteractor = sdk?.getSessionInteractor()
     }
 
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine);
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+            if (call.method == "getOAuthUrl") {
+                val oAuthUrl = sessionInteractor?.oauthUrl
+                oAuthUrl?.let {
+                    result.success(it)
+                } ?: run {
+                    result.error("UNAVAILABLE", "OAuth URL is not available.", null)
+                }
+            } else {
+                result.notImplemented()
+            }
+        }
     }
 }
--- GitFox Flutter Sample/lib/main.dart
+++ GitFox Flutter Sample/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 void main() => runApp(MyApp());
@@ -16,20 +17,49 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  static const platform = const MethodChannel('gitfox/oauth-url');
+  String _oAuthUrl = "";
+
+  @override
+  void initState() {
+    _retrieveOAuthUrl();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("GitFox Demo"),
       ),
-      body: WebView(
-        onWebViewCreated:
-          (webViewController) {
-            webViewController.loadUrl("https://yandex.ru");
-          }
-        ,
-      ),
+      body: Text(
+        "URL: $_oAuthUrl",
+      ) /*todo вернуть обратно WebView(
+        onWebViewCreated: (webViewController) {
+          webViewController.loadUrl("https://yandex.ru");
+        },
+      )*/
+      ,
     );
+  }
+
+  /// Retrieve OAuth URL from cross-platform SDK
+  Future<void> _retrieveOAuthUrl() async {
+    String oAuthUrl;
+    try {
+      oAuthUrl = await platform.invokeMethod('getOAuthUrl');
+    } on PlatformException catch (e) {
+      oAuthUrl = "";
+    }
+
+    setState(() {
+      _oAuthUrl = oAuthUrl;
+    });
   }
 }
