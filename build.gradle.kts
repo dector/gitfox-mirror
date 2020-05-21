232766a6cadf9d32e45f79c7695bbba644f344e1
terrakok <terrakok@gmail.com>
2020-05-21 17:41:23 +0300

    Update gradle and kotlin version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:4.1.0-alpha08")
-        val kotlinVersion = "1.3.70"
+        classpath("com.android.tools.build:gradle:4.1.0-alpha09")
+        val kotlinVersion = "1.3.72"
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
         classpath("org.jetbrains.kotlin:kotlin-serialization:$kotlinVersion")
     }
