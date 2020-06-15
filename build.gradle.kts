94bf4a4cf8d3ef9cd381bf3028fe1e06d178d3d0
terrakok <terrakok@gmail.com>
2020-06-15 13:19:17 +0300

    Downgrade to stable gradle version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:4.1.0-alpha10")
+        classpath("com.android.tools.build:gradle:4.0.0")
         val kotlinVersion = "1.3.72"
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
         classpath("org.jetbrains.kotlin:kotlin-serialization:$kotlinVersion")
